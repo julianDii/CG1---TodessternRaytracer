125e25030ae98c7558e724470fcbe919fd6382ed
Charline Waldrich <s59325@beuth-hochschule.de>
2015-10-20 12:10:20 +0200

    Arbeitseinteilung und Kommentare zu den Klassenköpfen hinzugefügt

--- src/raytracer/ImageSaver.java
+++ src/raytracer/ImageSaver.java
@@ -3,10 +3,36 @@
  */
 package raytracer;
 
+import javafx.application.Application;
+import javafx.scene.image.*;
+import javafx.stage.Stage;
+
 /**
- * @author rocko
+ * The Image Saver Class opens a window thanks to the JavaFX application and generates a black image pixel
+ * by pixel with a diagonal red line. The window size should be editable by the user. 
+ * 
+ * 
+ * @author Charline Waldrich
  *
  */
-public class ImageSaver {
+public class ImageSaver extends Application {
+	
 
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Image Saver");
+		primaryStage.setWidth(640);
+		primaryStage.setHeight(480);
+		primaryStage.show();
+		
+	}
+
+	public void draw(){
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);	
+	}
+	
 }
--- src/raytracer/ImageViewer.java
+++ src/raytracer/ImageViewer.java
@@ -2,11 +2,28 @@
  * 
  */
 package raytracer;
+import javafx.application.*;
+import javafx.stage.Stage;
 
 /**
- * @author rocko
+ * The class ImageViewer opens a window thanks to the FX Application and asks the User to choose 
+ * an image from his disk. After he has decided which, the window opens the same size as the 
+ * original picture. 
+ *  
+ * @author Robert Ullman
  *
  */
-public class ImageViewer {
+public class ImageViewer extends Application {
 
+
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("ImageViewer");
+		
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);	
+	}
+	
 }
