8213f8f36544c17783d895efcab9588c3f27658a
Charline Waldrich <s59325@beuth-hochschule.de>
2015-11-03 11:29:37 +0100

    Ray Class complished

--- src/raytracer/Ray.java
+++ src/raytracer/Ray.java
@@ -4,23 +4,35 @@ import matVecLib.Point3;
 import matVecLib.Vector3;
 
 /**
- * Created by Juliand on 03.11.15.
+ * Created by Charline Waldrich, 3.11.2015
  */
 public class Ray {
 
-    final Point3 o;
-    final Vector3 d;
+    private final Point3 o;
+    private final Vector3 d;
 
     public Ray(final Point3 o, final Vector3 d){
 
         this.o=o;
         this.d=d;
+        
     }
+    /** 
+     * This Method returns a Point with its coordinates after following the direction Vector 
+     * until we reach the given length.
+     * @param 	t is the given double value of the lenght we want the direction Vector to have
+     * @returns the point and its new coordinates
+     */
     public Point3 at(double t){
-        return null; //TODO
+        return o.add(d.mul(t));
     }
-    public double tOf(Point3 p){
-        return 0; //TODO
+    /**
+     * This Method is used to get the double value of the distance of two points.
+     * @param p is the Point of which we want to know the distance to our position vector of the ray
+     * @returns a double which is the distance of the given point p and o from each other 
+     */
+    public double tOf(Point3 p){ 
+    	return (p.sub(o)).magnitude;
     }
 
 }
