7177d46f88e0ba9d26abb60d17e349c8ea0798db
Charline Waldrich <s59325@beuth-hochschule.de>
2015-11-13 19:06:48 +0100

    Bild vom 1. Testcase in Latex Ordner hinzugefügt.

---
+++ src/ourWindow/NumberField.java
@@ -0,0 +1,14 @@
+package ourWindow;
+
+import javafx.scene.control.TextField;
+
+public class NumberField extends TextField{
+	
+	public NumberField(){
+		
+		setWidth(20);
+		setHeight(15);
+		
+	}
+
+}
