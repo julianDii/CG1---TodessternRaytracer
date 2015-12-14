37c7769f98aedc74ee7268cab09d9ed1a4a7085a
Charline Waldrich <s59325@beuth-hochschule.de>
2015-12-14 16:54:10 +0100

    NuberField laeuft jetzt ruuund

--- src/ourWindow/zusatzaufgabeGui/NumberField.java
+++ src/ourWindow/zusatzaufgabeGui/NumberField.java
@@ -7,34 +7,30 @@ import javafx.scene.control.TextField;
 public class NumberField extends TextField{
 
 	private static final String regex = "[+-]?[\\d]+[\\.]?[\\d]*";
-	private String initialText = "0";
 
 	public NumberField(){
-		setMaxSize(50, 10);
-		setText(initialText);
-		setOnMouseClicked(event1 -> this.clear());
-		setOnMouseExited(event2 -> setNumber());
+		this("0");
 	}
 
 	public NumberField(String text){
 		setMaxSize(50, 10);
 		setText(text);
 		setNumber();
-		setOnMouseClicked(event1 -> this.clear());
-		setOnMouseExited(event2 -> setNumber());
+		setOnMouseClicked(e -> this.clear());
+		focusedProperty().addListener(event2 -> setNumber());
 	}
 
 	private void setNumber(){
 		String text = getText();
-		if (!text.matches(regex) || text == null) {
-			this.setFocused(true);
-			this.setText(initialText);
+		if (!text.matches(regex)||text==null) {
+			this.setText("0");
 		} else {
-			this.initialText = text;
+			this.setText(text);
 		}
 	}
 
 	public double getNumber(){
-		return Double.parseDouble(initialText);
+		System.out.println(Double.parseDouble(getText()));
+		return Double.parseDouble(getText());
 	}
 }
